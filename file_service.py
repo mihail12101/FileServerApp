8dd6997b091288dcc33f1e44eb4b752616308da6
Mkalistratov <mkalistratov@luxoft.com>
2021-03-11 23:30:05 +0300

    Added 2 new functions
    - Added function "name_param_is_not_none" to check None value
    - Added function "merge_filename_with_root" to concatenate ROOT and given filename
    - Code updated with using new functions

--- file_service.py
+++ file_service.py
@@ -1,15 +1,13 @@
 import os
 
 from config import WORK_DIRECTORY
-from utils import generate_random_file_name
+from utils import generate_random_file_name, name_param_is_not_none, merge_filename_with_root
 
 
 def read_file(name=None):
-    if name is None:
-        raise ValueError("Parameter - name is absent or has wrong value")
+    name_param_is_not_none(name)
 
-    root = WORK_DIRECTORY
-    dst_path = os.path.join(root, name)
+    dst_path = merge_filename_with_root(name)
 
     if os.path.isfile(dst_path):
         with open(dst_path, "rt") as r_file:
@@ -19,28 +17,25 @@ def read_file(name=None):
 
 
 def delete_file(name=None):
-    if name is None:
-        raise ValueError("Parameter - name is absent or has wrong value")
-    
-    root = WORK_DIRECTORY
-    dst_path = os.path.join(root, name)
+    name_param_is_not_none(name)
+
+    dst_path = merge_filename_with_root(name)
 
     if os.path.isfile(dst_path):
         os.remove(dst_path)
 
 
 def create_file():
-    root = WORK_DIRECTORY
     file_name = generate_random_file_name()
-    dst_path = os.path.join(root, file_name)
+    dst_path = merge_filename_with_root(file_name)
+
     with open(dst_path, "wt") as new_file:
         new_file.write("some text")
 
-    # or return dst_path?
     return file_name
 
 
 def get_metadata(name=None):
-    root = WORK_DIRECTORY
-    dst_path = os.path.join(root, name)
+    name_param_is_not_none(name)
+    dst_path = merge_filename_with_root(name)
 
--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import os
 import random
 from string import ascii_letters, digits
 
-from config import FILENAME_LEN, FILE_EXTENSION
+from config import FILENAME_LEN, FILE_EXTENSION, WORK_DIRECTORY
 
 
 def get_path_from_arg(path):
@@ -14,3 +14,13 @@ def get_path_from_arg(path):
 
 def generate_random_file_name():
     return "".join(random.choice(ascii_letters + digits) for _ in range(FILENAME_LEN)) + FILE_EXTENSION
+
+
+def name_param_is_not_none(name=None):
+    if name is None:
+        raise ValueError("Parameter - name is absent or has wrong value")
+
+
+def merge_filename_with_root(name):
+    name_param_is_not_none()
+    return os.path.join(WORK_DIRECTORY, name)
