358a5e6d107f28231986c016def426f1c242d0d4
Mkalistratov <mkalistratov@luxoft.com>
2021-03-12 12:24:29 +0300

    - Added docstrings for each function in file_service.py

--- file_service.py
+++ file_service.py
@@ -4,6 +4,11 @@ from utils import generate_random_file_name, merge_filename_with_root, check_fil
 
 
 def read_file(name=None):
+    """Returns content from given file
+
+    - Param "name" should contain file name with file extension
+
+    """
     file_path = check_file_existence(name)
 
     with open(file_path, "rt") as r_file:
@@ -13,6 +18,10 @@ def read_file(name=None):
 
 
 def delete_file(name=None):
+    """Remove file if exists
+
+    - Param "name" should contain file name with file extension
+    """
     try:
         file_path = check_file_existence(name)
     except NameError:
@@ -22,6 +31,10 @@ def delete_file(name=None):
 
 
 def create_file():
+    """Create file with random file name
+
+    Returns string with file name and extension
+    """
     file_name = generate_random_file_name()
     dst_path = merge_filename_with_root(file_name)
 
@@ -32,5 +45,6 @@ def create_file():
 
 
 def get_metadata(name=None):
+    """Returns stat object with metadata inside"""
     file_path = check_file_existence(name)
     return os.stat(file_path)
