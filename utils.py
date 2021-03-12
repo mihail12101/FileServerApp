ceb78298e4a045f3274e368999f8b0d681bbc1c9
Mkalistratov <mkalistratov@luxoft.com>
2021-03-12 12:20:04 +0300

    - Added docstrings for each function in utils.py module

--- utils.py
+++ utils.py
@@ -2,10 +2,11 @@ import os
 import random
 from string import ascii_letters, digits
 
-from config import FILENAME_LEN, FILE_EXTENSION, WORK_DIRECTORY
+from config import FILENAME_LEN, FILE_EXTENSION, ENVVAR_ROOT
 
 
 def get_path_from_arg(path):
+    """Check that path given from argumets is directory"""
     if not os.path.isdir(path):
         raise NameError("Directory is not exists")
 
@@ -13,16 +14,23 @@ def get_path_from_arg(path):
 
 
 def generate_random_file_name():
+    """Generate random file names usign ascii symbols and digits
+
+    Return: string with filename + file extension
+    """
     return "".join(random.choice(ascii_letters + digits) for _ in range(FILENAME_LEN)) + FILE_EXTENSION
 
 
 def name_param_is_not_none(name=None):
+    """Check that parameter name is not none"""
     if name is None:
         raise ValueError("Parameter - name is absent or has wrong value")
 
 
 def check_file_existence(name=None):
     """ Check for file existence
+
+    - Param "name" should contain file name with file extension!
 
     * Checks that name param is not None
     * Builds path to provided file
@@ -40,6 +48,15 @@ def check_file_existence(name=None):
     return file_path
 
 
-def merge_filename_with_root(name):
-    name_param_is_not_none()
-    return os.path.join(WORK_DIRECTORY, name)
+def merge_filename_with_root(name=None):
+    """Common operations for work with files
+
+    - Param "name" should contain file name with file extension!
+
+    * Check that given filename is not None
+    * Getting WORK DIRECTORY from environment variables
+    * Concatanate WORK DIRECTORY and filename
+    """
+    name_param_is_not_none(name)
+    work_directory = os.path.normpath(os.getenv(ENVVAR_ROOT))
+    return os.path.join(work_directory, name)
